64f5e1ba6d20c103cc9295138a4f6bbd0b877155
Federico Airoldi <airoldibusiness@gmail.com>
2023-06-29 00:48:29 +0200

    FASE 1 - faccio la chimata api con axios

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,51 @@
-<script setup>
-import NameComp from "./components/NameComp.vue"
+<script>
+// import NameComp from "./components/NameComp.vue"
+import axios from "axios";
+
+export default{
+
+  data() {
+    return {
+
+      projects: [],
+      urlBase: 'http://127.0.0.1:8000'
+
+    }
+  },
+
+
+  mounted() {
+
+    this.getProjects();
+
+  },
+
+
+
+  methods: {
+
+    getProjects(){
+      axios.get(`${this.urlBase}/api/projects`)
+      .then( res => {
+        console.log(res.data);
+        this.projects = res.data.projects
+      })
+    }
+
+
+  }
+}
+
+
+
+
+
 
 </script>
 
 <template>
   
-  <h1 class="text-primary">Ciao Federico</h1>
-  <font-awesome-icon icon="fa-solid fa-rocket" />
-  <NameComp/>
+
   
 </template>
 
